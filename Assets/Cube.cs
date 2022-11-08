a592e7e86510f68e0e14fe0134eef56c441671ed
Codekus <markusklassen1497@yahoo.de>
2022-11-08 18:02:59 +0100

    BeatSaber test level

---
+++ Assets/Cube.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position +=  new Vector3(Time.deltaTime,1,1) + transform.forward ;
+        Vector3 velocity = new Vector3(0,0,1);
+        transform.Translate(velocity * Time.deltaTime * -2);
+        Debug.Log(transform.position);
+        
+    }
+}
---
+++ Assets/Saber.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saber : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public LayerMask layer;
+    private Vector3 previousPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1, layer))
+        {
+            Destroy(hit.transform.gameObject);
+            
+            /*
+            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
+            {
+                
+            }
+            */
+        }
+
+        previousPos = transform.position;
+    }
+}
