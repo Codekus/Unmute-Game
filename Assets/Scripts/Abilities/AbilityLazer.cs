4df6ce90b55bc957497f05510361a354af94321a
xatil <105444980+xatil@users.noreply.github.com>
2022-12-07 21:52:38 +0100

    added beam function (not complete)

--- Assets/Scripts/Abilities/AbilityLazer.cs
+++ Assets/Scripts/Abilities/AbilityLazer.cs
@@ -9,6 +9,8 @@ public class AbilityLazer : Ability
     private bool isRdy;
     private float timer = 0;
 
+    private LineRenderer rendo;
+
     [SerializeField] private Image _timerSprite;
     public override string getName()
     {
@@ -18,13 +20,16 @@ public class AbilityLazer : Ability
 
     public override bool isReady()
     {
-        throw new System.NotImplementedException();
+        return isRdy;
     }
 
     public override void use()
     {
         if (!isRdy) return;
         print("lazer used");
+        beam(transform.position, transform.forward, 5f);
+        rendo.enabled = true;
+        
         
         isRdy = false;
     }
@@ -34,6 +39,8 @@ public class AbilityLazer : Ability
     {
         isRdy = true;
         _timerSprite.fillAmount = 0;
+
+        rendo = gameObject.GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -50,5 +57,20 @@ public class AbilityLazer : Ability
             timer = 0;
             _timerSprite.fillAmount = timer;
         }
+    }
+
+    void beam(Vector3 targetPos, Vector3 direction, float length)
+    {
+        Ray ray = new Ray(targetPos, direction);
+        Vector3 endPos = targetPos + (direction * length);
+
+        if (Physics.Raycast(ray, out RaycastHit rayHit, length))
+        {
+            endPos = rayHit.point;
+            Debug.Log(rayHit.collider.gameObject.name);
+        }
+        
+        rendo.SetPosition(0, targetPos);
+        rendo.SetPosition(1, endPos);
     }
 }
