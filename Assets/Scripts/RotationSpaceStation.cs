021667e5923cb7057da07c12496bed7b5f313f16
xatil <105444980+xatil@users.noreply.github.com>
2023-01-15 18:34:38 +0100

    SpaceStation Rotation

--- Assets/Scripts/RotationSpaceStation.cs
+++ Assets/Scripts/RotationSpaceStation.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class RotationSpaceStation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.Rotate(0f,0f,0.01f);
     }
 }
